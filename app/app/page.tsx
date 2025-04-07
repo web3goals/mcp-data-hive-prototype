b7345b7f84897a15b2e6e4794e817f017e896cfc
kiv1n <aloha@kiv1n.ru>
2025-04-07 11:56:43 +0300

    Increase padding on landing page for improved layout

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function LandingPage() {
   return (
-    <main className="container mx-auto px-4 lg:px-80 py-12">
+    <main className="container mx-auto px-4 lg:px-80 py-16">
       <div className="flex flex-col items-center">
         <Image
           src="/images/cover.png"
